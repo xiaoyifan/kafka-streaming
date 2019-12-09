5e74b5894f694b3c9de510216d9ad70905b79127
Yifan Xiao <xiaoyifan@uchicago.edu>
2019-12-08 18:36:15 -0800

    update producer and weather rest proxy

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -38,9 +38,8 @@ class Producer:
         #
         #
         self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
+            "bootstrap.servers": "PLAINTEXT://localhost:9092",
+            "schema.registry.url": "http://localhost:8081"
         }
 
         # If the topic does not already exist, try to create it
@@ -49,8 +48,11 @@ class Producer:
             Producer.existing_topics.add(self.topic_name)
 
         # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+         self.producer = AvroProducer(
+            self.broker_properties,
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema
+         )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
@@ -58,21 +60,38 @@ class Producer:
         #
         # TODO: Write code that creates the topic for this producer if it does not already exist on
         # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+        client = AdminClient({
+            "bootstrap.servers": self.broker_properties["bootstrap.servers"]}
+        )
+
+        topic_metadata = client.list_topics(timeout = 5)
+
+        if self.topic_name in topic_metadata.topics:
+            logger.info(f"topic exisits: {self.topic_name}")
+            return
+
+        futures = client.create_topics([
+            NewTopic(
+                topic=self.topic_name, 
+                num_partitions=self.num_partitions, 
+                replication_factor=self.num_replicas)
+        ])
+        for topic, future in futures.items():
+            try:
+                future.result()
+                logger.info(f"topic {self.topic_name} created.")
+            except Exception as e:
+                logger.info(f"topic {self.topic_name} creation failed.")
+        
 
     def time_millis(self):
         return int(round(time.time() * 1000))
 
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
-        #
-        #
-        # TODO: Write cleanup code for the Producer here
-        #
-        #
-        logger.info("producer close incomplete - skipping")
+        if self.topic_name is not None:
+            self.producer.flush()
+            logger.info("producer shutdown.")
 
     def time_millis(self):
         """Use this function to get the key for Kafka Events"""
--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -13,13 +13,9 @@ logger = logging.getLogger(__name__)
 
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
-
-    #
-    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -40,17 +36,30 @@ class Turnstile(Producer):
         super().__init__(
             f"{station_name}", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
-            # TODO: value_schema=Turnstile.value_schema, TODO: Uncomment once schema is defined
-            # TODO: num_partitions=???,
-            # TODO: num_replicas=???,
+            value_schema=Turnstile.value_schema,
+            num_partitions=3,
+            num_replicas=2
         )
         self.station = station
         self.turnstile_hardware = TurnstileHardware(station)
+        # each station has 5 turnstile. 
+        self.turnstile_id = "turnstile_" + station_name + "_" + ''.join(random.randrange(5))
+        self.topic_name = self.turnstile_id
 
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
-        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
+        try: 
+            num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+            self.avro_producer.produce(
+            topic=self.topic_name,
+            key={"timestamp": self.time_millis()},
+            value={
+                "station_id": self.station.station_id, 
+                "station_name": self.station.station_name, 
+                "line": num_entries
+            })
+        except Exception as e: 
+           logger.info("Turnstile message for {} failed to write to topic {} with exception {}.".format(self.turnstile_id, self.topic_name, e))        
         #
         #
         # TODO: Complete this function by emitting a message to the turnstile topic for the number
